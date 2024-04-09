e47b7a3b1e52a7b6b16a38d8295b210a3a67346a
JoaoGDFaria <joaofaria13@gmail.com>
2024-04-09 16:39:12 +0100

    FichaAfericao

---
+++ Ficha_Aferição/Ficha_Aferição/controllers/pessoas.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { modelName } = require("../models/pessoas")
+var Pessoa = require("../models/pessoas")
+
+
+module.exports.list = () => {
+    return Pessoa
+        .find()
+        .sort({nome : 1})
+        .exec()
+}
+
+module.exports.findById = id => {
+    return Pessoa
+        .findOne({id : id})
+        .exec()
+}
+
+module.exports.insert = pessoa => {
+    if((Pessoa.find({id : pessoa.id}).exec()).length != 1){
+        var newPessoa = new Pessoa(pessoa)
+        return newPessoa.save()
+    }
+}
+
+module.exports.update = (id, pessoa) => {
+    return Pessoa
+        .findOneAndUpdate({id : id}, pessoa, {new : true})
+        .exec()
+}
+
+module.exports.remove = id => {
+    return Pessoa
+        .deleteOne({id : id})
+        .exec()
+}
---
+++ Ficha_Aferição/Ficha_Aferição/routes/pessoas.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var Pessoa = require('../controllers/pessoas')
+
+router.get('/', function(req, res, next) {
+    Pessoa.list()
+    .then(data =>{res.jsonp(data)})
+    .catch(function(erro){
+        res.status(501).render('error', {error: erro})
+    })
+})
+module.exports = router;
